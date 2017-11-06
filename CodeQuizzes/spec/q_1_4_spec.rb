1f5d6cd5896d5032e9cd4c6ac3e0b2ef65d3ae8f
Jakub <ku44@wp.pl>
2017-11-06 19:54:11 +0100

    Create q_1_4_spec.rb

---
+++ CodeQuizzes/spec/q_1_4_spec.rb
@@ -0,0 +1,9 @@
+require "q_1_4"
+
+describe "#vowels" do
+  it "returns array of words with vowels" do
+    str = "24 hour roadside resistance"
+    expected = %w|hour roadside resistance|
+    expect(vowels(str)).to eq expected
+  end
+end
